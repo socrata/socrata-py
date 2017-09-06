72778b443a264854c7b0a90a115c7c6e3fcecf04
Chris Duranti <chrisd1891@gmail.com>
2017-09-06 14:21:38 -0700

    fix example

--- examples/create.py
+++ examples/create.py
@@ -32,44 +32,16 @@ def path_to_show(resource):
     )
 
 def create(name, filepath):
-    publishing = Socrata(auth)
-
-    (ok, view) = publishing.views.create({'name': name})
-    print(ok)
-    print(view)
-    assert ok, view
-
-    (ok, revision) = view.revisions.update()
-    assert ok, revision
-
-    revision.open_in_browser()
-
-    (ok, upload) = revision.create_upload({'filename': path.basename(filepath)})
-    assert ok, upload
-
-    print("Created an Upload, accessible at: " + path_to_show(upload))
-
+    socrata = Socrata(auth)
     with open(filepath, 'rb') as csv_file:
-        (ok, input_schema) = upload.csv(csv_file)
-        assert ok, input_schema
+        (revision, output) = socrata.create(
+            name = name
+        ).csv(csv_file)
 
-    (ok, output_schema) = input_schema.latest_output()
-    assert ok, output_schema
+        (ok, job) = revision.apply(output_schema = output)
+        assert ok, job
 
-    (ok, output_schema) = output_schema.wait_for_finish()
-    assert ok, output_schema
+        revision.open_in_browser()
 
-    print("Schema has completed, accessible at: " + path_to_show(output_schema))
-    print("Found {error_count} validation errors as the result of applying transforms".format(
-        error_count = output_schema.attributes['error_count']
-    ))
-
-    (ok, job) = revision.apply(output_schema = output_schema)
-    assert ok, job
-    print("Started upsert, job is viewable at" + path_to_show(job))
-
-    job.wait_for_finish(progress = lambda job: sys.stdout.write(str(job.attributes['log'][0]) + "\n"))
-
-    print("Your view is ready: {url}".format(url = view.ui_url()))
 
 create(args.name, args.csv)

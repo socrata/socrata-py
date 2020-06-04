698f8acf030a8c2c5399526f53d64f650a6f4e1b
rozap <chrisd1891@gmail.com>
2020-06-04 10:33:15 -0700

    Two issues
    * any_failed was recursive
    * upsert test only waited ~10s, so slow staging
      could cause it to fail

--- test/upsert_job_test.py
+++ test/upsert_job_test.py
@@ -7,14 +7,6 @@ class TestUpsertJob(TestCase):
         input_schema = self.create_input_schema(rev = rev)
         output_schema = self.create_output_schema(input_schema = input_schema)
         job = rev.apply(output_schema = output_schema)
-        done = False
-        attempts = 0
-        while not done and attempts < 20:
-            job = job.show()
-            attempts += 1
-
-            done = job.attributes['status'] == 'successful'
-            sleep(0.5)
-
-        assert done, "Polling job never resulted in a successful completion: %s" % job
+        job = job.wait_for_finish()
+        job.attributes['status'] == 'successful'
 

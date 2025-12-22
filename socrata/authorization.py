a830fc152f0c46728cc5695e8843848c471cc6cc
David Torres <david.torres@tylertech.com>
2025-12-22 11:38:18 -0800

    Combine authorizations

--- socrata/authorization.py
+++ socrata/authorization.py
@@ -6,15 +6,34 @@ class Authorization(object):
     This is passed into the `Socrata` object once, which is the entry
     point for all operations.
 
+    Can be used with either username/password or cookies. If a user
+    name and password are provided, HTTP Basic Auth will be used. Otherwise,
+    cookies will be used for authentication.
+
+    Username/password example:
         auth = Authorization(
             "data.seattle.gov",
-            os.environ['SOCRATA_USERNAME'],
-            os.environ['SOCRATA_PASSWORD']
+            username=os.environ['SOCRATA_USERNAME'],
+            password=os.environ['SOCRATA_PASSWORD']
+        )
+        publishing = Socrata(auth)
+
+    Cookies example:
+        auth = Authorization(
+            "data.seattle.gov",
+            cookies=dict(cookie_name="cookie-value")
         )
         publishing = Socrata(auth)
     """
-    def __init__(self, domain, username, password, request_id_prefix = ''):
+    def __init__(self, domain, /, username=None, password=None, request_id_prefix='', *, cookies=None):
         self.domain = domain
+
+        if not ((username and password) or cookies):
+            raise ValueError("Either username/password or cookies must be provided")
+
+        if cookies and not isinstance(cookies, dict):
+            raise TypeError("cookies must be a dictionary")
+
         self.username = username
         self.password = password
 
@@ -23,8 +42,13 @@ class Authorization(object):
 
         self._request_id_prefix = request_id_prefix
 
-        self.basic = HTTPBasicAuth(self.username, self.password)
-        self.cookies = dict()
+        # Set up authentication method
+        if username and password:
+            self.basic = HTTPBasicAuth(self.username, self.password)
+            self.cookies = dict()
+        else:
+            self.basic = None
+            self.cookies = cookies or dict()
 
     def live_dangerously(self):
         """
@@ -38,40 +62,3 @@ class Authorization(object):
 
     def request_id_prefix(self):
         return self._request_id_prefix
-
-class CookieAuthorization(object):
-    """
-    Manages basic authorization for accessing the socrata API.
-    This is passed into the `Socrata` object once, which is the entry
-    point for all operations.
-
-        auth = CookieAuthorization(
-            "data.seattle.gov",
-            dict(cookie_name="cookie-value")
-        )
-        publishing = Socrata(auth)
-    """
-    def __init__(self, domain, cookies, request_id_prefix = ''):
-        self.domain = domain
-        self.cookies = cookies
-
-        self.proto = 'https://'
-        self.verify = True
-
-        self._request_id_prefix = request_id_prefix
-
-        self.basic = None
-
-    def live_dangerously(self):
-        """
-        Disable SSL checking. Note that this should *only* be used while developing
-        against a local Socrata instance.
-        """
-        import requests
-        from requests.packages.urllib3.exceptions import InsecureRequestWarning
-        requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-        self.verify = False
-
-    def request_id_prefix(self):
-        return self._request_id_prefix
-
